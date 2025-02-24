997dbc799b1ddca6625f4fc2b7a1b002fb4515fb
FatmaQunnies24 <fatima.n.qunnies@gmail.com>
2025-02-24 15:43:48 +0200

    edit role

--- lib/Admin/AdminProfileSectio/editRole.dart
+++ lib/Admin/AdminProfileSectio/editRole.dart
@@ -4,6 +4,10 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../Card/Card.dart';
+import '../../Home_Section/home.dart';
+import '../../Product/productPage.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -29,7 +33,7 @@ class UserFilterPage extends StatefulWidget {
 class _UserFilterPageState extends State<UserFilterPage> {
   final TextEditingController _searchController = TextEditingController();
   List<dynamic> _users = [];
-  Map<String, String?> _selectedRoles = {}; // لتخزين الرول المحدد لكل مستخدم
+  Map<String, String?> _selectedRoles = {};
 
   Future<Object> _getUserInfo() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -55,7 +59,10 @@ class _UserFilterPageState extends State<UserFilterPage> {
     if (response.statusCode == 200) {
       setState(() {
         _users = jsonDecode(response.body);
-        _selectedRoles = {}; // إعادة تعيين الأدوار المحددة عند البحث
+        _selectedRoles = {};
+        for (var user in _users) {
+          _selectedRoles[user['id']] = user['role'];
+        }
       });
     } else {
       throw Exception('Failed to load users');
@@ -86,15 +93,15 @@ class _UserFilterPageState extends State<UserFilterPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('User Filter'),
-        backgroundColor: Colors.transparent, // جعل شريط التطبيق شفافًا
-        elevation: 0, // إزالة الظل
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
-      extendBodyBehindAppBar: true, // لجعل الخلفية تمتد خلف شريط التطبيق
+      extendBodyBehindAppBar: true,
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
             image: NetworkImage(
-              'https://res.cloudinary.com/davwgirjs/image/upload/v1740399050/nhndev/product/Atn5pCQF7VR4KhJCzI4g_1740399048265_005bdf0b-38be-4cbd-a016-f0c574659898.jpg.jpg',
+              'https://res.cloudinary.com/davwgirjs/image/upload/v1740398445/nhndev/product/Atn5pCQF7VR4KhJCzI4g_1740398442198_aboutusbg.jpg.jpg',
             ),
             fit: BoxFit.cover,
           ),
@@ -103,9 +110,8 @@ class _UserFilterPageState extends State<UserFilterPage> {
           filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
           child: Column(
             children: [
-              // العنوان وحقل البحث
               Padding(
-                padding: const EdgeInsets.only(top: 100.0, left: 16.0, right: 16.0), // تباعد من الأعلى
+                padding: const EdgeInsets.only(top: 100.0, left: 16.0, right: 16.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -113,11 +119,11 @@ class _UserFilterPageState extends State<UserFilterPage> {
                       'User Filter',
                       style: TextStyle(
                         color: Colors.white,
-                        fontSize: 24,
+                        fontSize: 28,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(height: 16), // مسافة بين العنوان وحقل البحث
+                    SizedBox(height: 20),
                     Row(
                       children: [
                         Expanded(
@@ -126,18 +132,33 @@ class _UserFilterPageState extends State<UserFilterPage> {
                             decoration: InputDecoration(
                               labelText: 'Search by username',
                               filled: true,
-                              fillColor: Colors.black.withOpacity(0.5), // خلفية سوداء شفافة
-                              labelStyle: TextStyle(color: Colors.white), // لون النص الأبيض
+                              fillColor: Colors.black.withOpacity(0.5),
+                              labelStyle: TextStyle(color: Colors.white),
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(10.0),
                               ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.blue),
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
                             ),
-                            style: TextStyle(color: Colors.white), // لون النص الأبيض
+                            style: TextStyle(color: Colors.white),
                           ),
                         ),
-                        IconButton(
-                          icon: Icon(Icons.search, color: Colors.white), // لون الأيقونة أبيض
-                          onPressed: _searchUsers,
+                        SizedBox(width: 10),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: IconButton(
+                            icon: Icon(Icons.search, color: Colors.white),
+                            onPressed: _searchUsers,
+                          ),
                         ),
                       ],
                     ),
@@ -152,21 +173,21 @@ class _UserFilterPageState extends State<UserFilterPage> {
                     return Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
                       child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0), // زوايا مستديرة
+                        borderRadius: BorderRadius.circular(10.0),
                         child: BackdropFilter(
                           filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
                           child: Container(
-                            color: Colors.black.withOpacity(0.5), // خلفية سوداء شفافة
+                            color: Colors.black.withOpacity(0.5),
                             child: ListTile(
                               title: Text(
                                 user['userName'],
-                                style: TextStyle(color: Colors.white), // لون النص الأبيض
+                                style: TextStyle(color: Colors.white, fontSize: 18),
                               ),
                               trailing: DropdownButton<String>(
                                 value: _selectedRoles[user['id']],
                                 hint: Text(
                                   'Select Role',
-                                  style: TextStyle(color: Colors.white), // لون النص الأبيض
+                                  style: TextStyle(color: Colors.white),
                                 ),
                                 onChanged: (String? newValue) {
                                   setState(() {
@@ -174,14 +195,14 @@ class _UserFilterPageState extends State<UserFilterPage> {
                                   });
                                   _updateUserRole(user['id'], newValue!);
                                 },
-                                dropdownColor: Colors.black.withOpacity(0.8), // خلفية القائمة المنسدلة
+                                dropdownColor: Colors.black.withOpacity(0.8),
                                 items: <String>['USER', 'SKIN_EXPERT', 'ADMIN']
                                     .map<DropdownMenuItem<String>>((String value) {
                                   return DropdownMenuItem<String>(
                                     value: value,
                                     child: Text(
                                       value,
-                                      style: TextStyle(color: Colors.white), // لون النص الأبيض
+                                      style: TextStyle(color: Colors.white),
                                     ),
                                   );
                                 }).toList(),
@@ -198,6 +219,135 @@ class _UserFilterPageState extends State<UserFilterPage> {
           ),
         ),
       ),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        token: 'your_token_here', // Replace with your actual token
+        userInfo: {}, // Replace with your actual user info
+      ),
     );
   }
 }
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  final String token;
+  final Map<String, dynamic> userInfo;
+
+  const CustomBottomNavigationBar({
+    super.key,
+    required this.token,
+    required this.userInfo,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        ClipPath(
+          clipper: BottomWaveClipper(),
+          child: Container(
+            height: 70,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 10,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 25,
+          left: MediaQuery.of(context).size.width / 2 - 30,
+          child: FloatingActionButton(
+            backgroundColor: Colors.blue,
+            onPressed: () {},
+            child: const Icon(Icons.face, color: Colors.white),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            height: 70,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.home, color: Colors.blue),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Home(token: token),
+                      ),
+                    );
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.chat, color: Colors.blue),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MessageCard(token: token),
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(width: 60),
+                IconButton(
+                  icon: const Icon(Icons.settings, color: Colors.blue),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ProductPage(token: token, userInfo: userInfo),
+                      ),
+                    );
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.person, color: Colors.blue),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class BottomWaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0.0, size.height - 20);
+
+    var firstControlPoint = Offset(size.width / 4, size.height);
+    var firstEndPoint = Offset(size.width / 2.25, size.height - 30.0);
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+        firstEndPoint.dx, firstEndPoint.dy);
+
+    var secondControlPoint =
+    Offset(size.width - (size.width / 3.25), size.height - 65);
+    var secondEndPoint = Offset(size.width, size.height - 40);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondEndPoint.dx, secondEndPoint.dy);
+
+    path.lineTo(size.width, size.height - 40);
+    path.lineTo(size.width, 0.0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+
